d2693980c36ae50fe2716b818bbc30192c238f3f
cc189 <cc189dev@gmail.com>
2020-01-13 23:46:22 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import * as React from "react";
 import 'antd/dist/antd.css';
 
-import { LayoutContainer } from "./containers/LayoutContainer";
+import LayoutContainer from "./containers/LayoutContainer";
 
+type Props = {
+    stepOptions: any[]
+    onSaved: any
+    stepDbClick: any
+}
 
-export default class ProcessEditor extends React.Component<{}, {}> {
+export default class ProcessEditor extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
     render() {
         return (
-            <div style={{height: `100%`}}>
-                <LayoutContainer/>
+            <div style={{ height: `100%` }}>
+                <LayoutContainer {...this.props} />
             </div>
         )
     }
