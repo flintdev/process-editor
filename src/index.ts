fb1942c4f42b46b59edc0d96534b2b66be76e94c
cc189 <cc189dev@gmail.com>
2020-02-18 14:24:44 -0800

    Add EditorData interface

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
 import ProcessEditor from "./ProcessEditor";
 
-export default ProcessEditor;
+interface EditorNodeOutput {
+    name: string,
+    condition?: {
+        key?: string,
+        operator?: string,
+        value?: string,
+    }
+}
+
+interface EditorNodeConnection {
+    node: number | string
+    input: string
+}
+
+interface EditorNode {
+    id: number | string,
+    name: string,
+    position: number[], // [x, y]
+    outputs: {
+        [key: string]: EditorNodeConnection
+    },
+    inputs: {
+        [key: string]: EditorNodeConnection
+    },
+    data: {
+        label: string
+        type: string
+        group: string
+        category: string
+        code: string,
+        outputs: Array<EditorNodeOutput>
+    }
+}
+
+interface EditorData {
+    id: string,
+    nodes: {
+        [key: string]: EditorNode
+    }
+}
+
+export {ProcessEditor, EditorData, EditorNode, EditorNodeConnection, EditorNodeOutput};
